ca3744ca757fc50dfd4e37619c51e3a3f937bc18
habatoo <habatoo@gmail.com>
2021-08-31 21:01:23 +0700

    initial commit

---
+++ src/main/java/Laptenkov/Product.java
@@ -0,0 +1,19 @@
+package Laptenkov;
+
+/**
+ * Класс Product характеризует объект - продовольственные продукт
+ * Поля:
+ * - id
+ * - название
+ * - описание
+ * - срок годности
+ * - производитель
+ * - List<String> штрихкоды
+ *
+ * Методы:
+ *  public String serializeProductAsJSON(Product product) throws Exception;
+ *
+ * public Product deserializeProductFromJSON(String str) throws Exception;
+ */
+public class Product {
+}
---
+++ src/main/java/Laptenkov/SerializationService.java
@@ -0,0 +1,25 @@
+package Laptenkov;
+
+import java.io.IOException;
+
+/**
+ * Класс SerializationService,
+ * обеспечивает сериализацию и десериализацию объектов Product в JSON
+ *
+ * Для сериализации / десериализации в JSON использовать класс ObjectMapper
+ * Для сериализации / десериализации в XML использовать класс JAXBContext
+ */
+public class SerializationService {
+
+    public String serializeProductAsJSON(Product product) throws Exception {
+
+        // you should implement
+        return null;
+    }
+
+    public Product deserializeProductFromJSON(String str) throws Exception {
+
+        // you should implement
+        return null;
+    }
+}
